80cc9f2c045279fae00c53edeb87992824dc8c6f
jeongsam3659 <57051148+jeongsam3659@users.noreply.github.com>
2022-07-07 02:06:13 +0900

    css 링크 등록
    설명서 필요함.

---
+++ git/scss-masterclass-master/gulpfile.babel.js
@@ -0,0 +1,46 @@
+import gulp from "gulp";
+import del from "del";
+import sass from "gulp-sass";
+import minify from "gulp-csso";
+import autoprefixer from "gulp-autoprefixer";
+
+sass.compiler = require("node-sass");
+
+const routes = {
+  css: {
+    watch: "src/scss/*",
+    src: "src/scss/styles.scss", 
+    /* 대상이 될 파일 */
+    /* 일어나는 모든 일은 css로 compile되는것. */
+    dest: "dist/css"
+    /* 컴파일 후 css에 표현될 곳. */
+  }
+};
+
+const styles = () =>
+  gulp
+    .src(routes.css.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        flexbox: true,
+        grid: "autoplace"
+      })
+    )
+    .pipe(minify())
+    .pipe(gulp.dest(routes.css.dest));
+
+const watch = () => {
+  gulp.watch(routes.css.watch, styles);
+};
+
+const clean = () => del(["dist/styles.css"]);
+                        /* 컴파일 후 css에 표현될 곳. */
+
+const prepare = gulp.series([clean]);
+
+const assets = gulp.series([styles]);
+
+const live = gulp.parallel([watch]);
+
+export const dev = gulp.series([prepare, assets, live]);
